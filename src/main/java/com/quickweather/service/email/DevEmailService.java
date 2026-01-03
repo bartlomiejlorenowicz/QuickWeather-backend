678068f477df6a219de872caaf13c3151ef90e70
Bartlomiej <bartlomiejlorenowicz@gmail.com>
2026-01-03 21:41:51 +0100

    refactor: remove 'local' profile from DevEmailService annotations

--- src/main/java/com/quickweather/service/email/DevEmailService.java
+++ src/main/java/com/quickweather/service/email/DevEmailService.java
@@ -5,7 +5,7 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
 @Service
-@Profile({"dev", "local"})
+@Profile({"dev"})
 @Slf4j
 public class DevEmailService implements EmailService {
 
